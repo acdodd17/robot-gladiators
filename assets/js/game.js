fccab9a436976d75eec88021eeed99a1dfa73398
Anna <acdodd17@gmail.com>
2021-10-19 20:32:32 -0400

    new Rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,13 +16,10 @@ var enemyNames = ["Roborto" , "Amy Android" , "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-//Alert players that they are starting the round
-window.alert("Welcome to Robot Gladiators!");
-
 
 var fight = function(enemyName) {
 
-    while (enemyHealth > 0) {
+    while (enemyHealth > 0 && playerHealth > 0) {
         
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -43,6 +40,7 @@ var fight = function(enemyName) {
                 // check enemy's health
                 if (enemyHealth <= 0) {
                     window.alert(enemyName + " has died!");
+                    break;
                 } 
                 else {
                 window.alert(enemyName + " still has " + enemyHealth + " health left.");
@@ -59,6 +57,7 @@ var fight = function(enemyName) {
                 // check player's health
                 if (playerHealth <= 0) {
                     window.alert(playerName + " has died!");
+                    break;
                 } 
                 else {
                     window.alert(playerName + " still has " + playerHealth + " health left.");
@@ -74,7 +73,8 @@ var fight = function(enemyName) {
                     window.alert(playerName + " has decided to skip this fight. Goodbye!");
                     // subtract money from playerMoney for skipping
                     playerMoney = playerMoney - 2; 
-                    console.log(playerMoney);
+                    console.log("playerMoney" , playerMoney);
+                    break;
                 }
                 //if no (false), ask question again by running fight() again
                 else {
@@ -88,11 +88,19 @@ var fight = function(enemyName) {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        //Alert players that they are starting the round
+        window.alert("Welcome to Robot Gladiators! " + "Round " + ( i + 1 ));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        debugger; 
+        fight(pickedEnemyName);
+    }
+
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
+
 }
-
-
-
 
